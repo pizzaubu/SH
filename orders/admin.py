3a31c8937a4b7fbf02a0a4934b074c41c2ff9790
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2023-03-18 04:10:50 +0700

    add orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
-
+from .models import Payment, Order, OrderProduct, Coupon, Refund
 # Register your models here.
+
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered')
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'is_ordered', 'created_at']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone', 'email']
+    list_per_page = 20
+    inlines = [OrderProductInline]
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'discount', 'valid_from', 'valid_to', 'is_active']
+    list_filter = ['is_active', 'valid_from', 'valid_to']
+    search_fields = ['code']
+    list_per_page = 20
+
+class RefundAdmin(admin.ModelAdmin):
+    list_display = ['order', 'reason', 'accepted', 'email']
+    list_filter = ['accepted', 'order']
+    search_fields = ['order__order_number', 'email']
+    list_per_page = 20
+
+admin.site.register(Refund, RefundAdmin)
+admin.site.register(Coupon, CouponAdmin)
+admin.site.register(Payment)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct)
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Product, ReviewRating, Coupon
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+    # เพิ่มความสามารถในการกรองข้อมูล
+    list_filter = ('category', 'is_available')
+
+    # เพิ่มความสามารถในการค้นหาข้อมูล
+    search_fields = ('product_name', 'category__category_name')
+
+    # เพิ่มความสามารถในการเรียงลำดับข้อมูล
+    list_per_page = 20
+    ordering = ('-modified_date',)
+
+# สำหรับ CouponAdmin เราจะใช้ list_display เพื่อเลือกแสดงข้อมูลที่ต้องการในรายการ
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'valid_from', 'valid_to', 'discount', 'is_active')
+
+    # เพิ่มความสามารถในการกรองข้อมูล
+    list_filter = ('is_active',)
+
+    # เพิ่มความสามารถในการค้นหาข้อมูล
+    search_fields = ('code',)
+
+    # เพิ่มความสามารถในการเรียงลำดับข้อมูล
+    list_per_page = 20
+    ordering = ('-valid_to',)
+
+# สำหรับ ReviewRatingAdmin เราจะใช้ list_display เพื่อเลือกแสดงข้อมูลที่ต้องการในรายการ
+class ReviewRatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'rating', 'review', 'status', 'created_at')
+
+    # เพิ่มความสามารถในการกรองข้อมูล
+    list_filter = ('status', 'rating')
+
+    # เพิ่มความสามารถในการค้นหาข้อมูล
+    search_fields = ('user__username', 'product__product_name')
+
+    # เพิ่มความสามารถในการเรียงลำดับข้อมูล
+    list_per_page = 20
+    ordering = ('-created_at',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ReviewRating, ReviewRatingAdmin)
+admin.site.register(Coupon, CouponAdmin)

c8b5b377d47a294aa0199a690fabce2a664a999a
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2023-08-13 11:53:22 +0700

    fix static in setting

--- SH/settings.py
+++ SH/settings.py
@@ -139,10 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-"""STATICFILES_DIRS = [
+STATIC_ROOT = os.path.join(BASE_DIR, 'SH/static')
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-]"""
+]
 
 # media files configuration
 MEDIA_URL = '/media/'
